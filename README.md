e03fbb71981f968e77f204dd78258c810c6110db
Melechtna Antelecht <melechtna@Mels-PC.lan>
2024-03-05 03:21:20 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# Simple Controller Framework (SCF)
+
+SCF, as it's name implies, is a framework written with the html5-gamepad API to allow you to create a standalone, green screen'd window that you can add to softwares like OBS so that your input can be displayed. The main reason I created this framework is two fold, one, to handle Flatpak issues where the controller can't be seen, and two, so that you can easily import any controller you want, and have it look like your ACTUAL controller.
+
+The code is as human readable as possible, so that it should be as simple as possible to understand what does what, and what needs changing to fit your controller, if anything. There will be multiple examples provided, including some non-standard ones, and I will be adding as many as I have access too.
+
+# Structure
+
+The sctructure should be as follows, controller folder, manufacturer folder, model folder, color folder (if applicable), icons, images, and scripts folder.
+
+## Controllers
+
+The controllers folder will contain sub folders for each respective manufacturer.
+
+## Manufacturer
+
+The Manufacturer folder(s) will contain a subfolder with the controllers respective model name as its own folder, if this is the end of the line (EotL), the index.html will go in here.
+
+## Colors
+
+The Colors folder(s), if applicable, will contain the coloration for each respective coloration for that respective model for that respective controller. If this is the end of the line (EotL), the index.html will go in here.
+
+## icons, images, and scripts
+
+These three folders will be the ones that actually contain the "meat" of what makes up the controller itself, the icons folder will contain one, or multiple (to yours or my preference), icons for the "site" to use. The images folder is what actually makes up your controller, there will be a base image, and an image for each and every button that makes up the controller. It's usually best to start with the base and any floating buttons you may need to display the entire controller. It's best if you can keep everything "small", by this I mean, if you can get away with using less space, do so, but every image needs to be the same resolution, but the majority of each buttons canvas should be transparent, see premade controllers for examples.
+
+You will also want to create pressed states for the buttons, see any of the premade controllers for examples which are designated with the ending letter P, before the png extension. This allows the code to be efficent, by simply swapping these images out. It also makes it very easy for anyone to simply change the color of the pressed state with a fill tool in just about any image editor imaginable, to get their desired color. Lastly is the scripts folder, the only thing that goes here is renderer.js, this script does all the heavy lifting.
+
+# Creating your own
+
+For the most part, the process to create the images is quite simple, it's just tedious, and I will be checking any submissions to meet "minimum quality", basically this just means, you bothered to take the time to make the buttons properly detached, aligned the floating buttons/triggers well enough, that sort of thing, not say, you merely square cut the buttons out and slapped some paint over the buttons or something like that.
+
+Where it may get a bit challenging, is properly editing the index.html to properly reflect the controller, its icon, and pointing it towards the correct button images to pass to the renderer. The renderer itself is where it gets the most complicated, and there's no easy "do this" recommendation, as it will largely depend on the controller itself and what's required to make it work. Your best bet is to look at the pre-made examples, and work things out yourself, or, if you can either send me the controller, or are more willing to work with me on debugging it, I may be able to do so myself given the time.
+
+# Running
+
+Simply locate the folder of the controller you want, and open index.html found in that controllers respective EotL, in any web browser that has html5-gamepad enabled, which should be most besides Firefox from my experience, this of course is subject to change, as Firefox may add this later.
---
+++ controllers/retro-bit/SEGA Genesis/Black/scripts/renderer.js
@@ -0,0 +1,289 @@
+//Button List
+const aButtonImage = document.getElementById('a-button');
+const bButtonImage = document.getElementById('b-button');
+const cButtonImage = document.getElementById('c-button');
+const xButtonImage = document.getElementById('x-button');
+const yButtonImage = document.getElementById('y-button');
+const zButtonImage = document.getElementById('z-button');
+const startButtonImage = document.getElementById('start-button');
+const selectButtonImage = document.getElementById('mode-button');
+const leftBumperImage = document.getElementById('left-bumper');
+const rightBumperImage = document.getElementById('right-bumper');
+const dPadUpImage = document.getElementById('dpad-up');
+const dPadUpRightImage = document.getElementById('dpad-up-right');
+const dPadUpLeftImage = document.getElementById('dpad-up-left');
+const dPadRightImage = document.getElementById('dpad-right');
+const dPadDownLeftImage = document.getElementById('dpad-down-left');
+const dPadDownRightImage = document.getElementById('dpad-down-right');
+const dPadDownImage = document.getElementById('dpad-down');
+const dPadLeftImage = document.getElementById('dpad-left');
+
+// Function to handle gamepad connected event
+function handleGamepadConnected(event) {
+    requestAnimationFrame(checkGamepad);
+}
+
+// Function to update image based on A button state
+function updateAButtonImage(isPressed) {
+    if (isPressed) {
+        aButtonImage.src = 'images/AP.png';
+    } else {
+        aButtonImage.src = 'images/A.png';
+    }
+}
+
+// Function to update image based on B button state
+function updateBButtonImage(isPressed) {
+    if (isPressed) {
+        bButtonImage.src = 'images/BP.png';
+    } else {
+        bButtonImage.src = 'images/B.png';
+    }
+}
+
+// Function to update image based on B button state
+function updateCButtonImage(isPressed) {
+    if (isPressed) {
+        cButtonImage.src = 'images/CP.png';
+    } else {
+        cButtonImage.src = 'images/C.png';
+    }
+}
+
+// Function to update image based on X button state
+function updateXButtonImage(isPressed) {
+    if (isPressed) {
+        xButtonImage.src = 'images/XP.png';
+    } else {
+        xButtonImage.src = 'images/X.png';
+    }
+}
+
+// Function to update image based on Y button state
+function updateYButtonImage(isPressed) {
+    if (isPressed) {
+        yButtonImage.src = 'images/YP.png';
+    } else {
+        yButtonImage.src = 'images/Y.png';
+    }
+}
+
+// Function to update image based on Y button state
+function updateZButtonImage(isPressed) {
+    if (isPressed) {
+        zButtonImage.src = 'images/ZP.png';
+    } else {
+        zButtonImage.src = 'images/Z.png';
+    }
+}
+
+// Function to update image based on Start button state
+function updateStartButtonImage(isPressed) {
+    if (isPressed) {
+        startButtonImage.src = 'images/StartP.png';
+    } else {
+        startButtonImage.src = 'images/Start.png';
+    }
+}
+
+// Function to update image based on Select button state
+function updateSelectButtonImage(isPressed) {
+    if (isPressed) {
+        selectButtonImage.src = 'images/SelectP.png';
+    } else {
+        selectButtonImage.src = 'images/Select.png';
+    }
+}
+
+// Function to update image based on Left bumper state
+function updateLeftBumperImage(isPressed) {
+    if (isPressed) {
+        leftBumperImage.src = 'images/L1P.png';
+    } else {
+        leftBumperImage.src = 'images/L1.png';
+    }
+}
+
+// Function to update image based on Right bumper state
+function updateRightBumperImage(isPressed) {
+    if (isPressed) {
+        rightBumperImage.src = 'images/R1P.png';
+    } else {
+        rightBumperImage.src = 'images/R1.png';
+    }
+}
+
+// Function to update D-pad Up button state
+function updateDPadUpButton(isPressed) {
+    if (isPressed) {
+        dPadUpImage.src = 'images/DUP.png';
+    } else {
+        dPadUpImage.src = 'images/DU.png';
+    }
+}
+
+// Function to update D-pad Up Right button state
+function updateDPadUpRightButton(isPressed) {
+    if (isPressed) {
+        dPadUpRightImage.src = 'images/DURP.png';
+    } else {
+        dPadUpRightImage.src = 'images/DUR.png';
+    }
+}
+
+// Function to update D-pad Right button state
+function updateDPadRightButton(isPressed) {
+    if (isPressed) {
+        dPadRightImage.src = 'images/DRP.png';
+    } else {
+        dPadRightImage.src = 'images/DR.png';
+    }
+}
+
+// Function to update D-pad Down Right button state
+function updateDPadDownRightButton(isPressed) {
+    if (isPressed) {
+        dPadDownRightImage.src = 'images/DDRP.png';
+    } else {
+        dPadDownRightImage.src = 'images/DDR.png';
+    }
+}
+
+// Function to update D-pad Down button state
+function updateDPadDownButton(isPressed) {
+    if (isPressed) {
+        dPadDownImage.src = 'images/DDP.png';
+    } else {
+        dPadDownImage.src = 'images/DD.png';
+    }
+}
+
+// Function to update D-pad Up Left button state
+function updateDPadUpLeftButton(isPressed) {
+    if (isPressed) {
+        dPadUpLeftImage.src = 'images/DULP.png';
+    } else {
+        dPadUpLeftImage.src = 'images/DUL.png';
+    }
+}
+
+// Function to update D-pad Left button state
+function updateDPadLeftButton(isPressed) {
+    if (isPressed) {
+        dPadLeftImage.src = 'images/DLP.png';
+    } else {
+        dPadLeftImage.src = 'images/DL.png';
+    }
+}
+
+// Function to update D-pad Down Left button state
+function updateDPadDownLeftButton(isPressed) {
+    if (isPressed) {
+        dPadDownLeftImage.src = 'images/DDLP.png';
+    } else {
+        dPadDownLeftImage.src = 'images/DDL.png';
+    }
+}
+
+
+// Function to check and update gamepad state
+function checkGamepad() {
+    const gamepads = navigator.getGamepads();
+
+    // Ensure there's at least one non-null gamepad
+    const gamepad = gamepads.find(pad => pad !== null);
+
+    if (gamepad) {
+        console.log("Gamepad found:", gamepad);
+
+        // Replace these values with the actual vendor and product IDs of your working controller
+        const desiredVendorId = "0079";
+        const desiredProductId = "0011";
+
+        // Extract vendor and product information from the id string
+        const match = /Vendor: (\w+) Product: (\w+)/.exec(gamepad.id);
+        const vendorId = match ? match[1] : undefined;
+        const productId = match ? match[2] : undefined;
+
+        console.log("Vendor ID:", vendorId, "Product ID:", productId);
+
+        // Check if the ID matches the desired controller
+        if (vendorId === desiredVendorId && productId === desiredProductId) {
+            console.log("Matching controller detected!");
+
+            // Check DPad
+            const XAxisValue = gamepad.axes[0];
+            const YAxisValue = gamepad.axes[1];
+
+            // Check left bumper
+            const leftBumperValue = gamepad.buttons[6].pressed;
+            updateLeftBumperImage(leftBumperValue);
+
+            // Check right bumper
+            const rightBumperValue = gamepad.buttons[7].pressed;
+            updateRightBumperImage(rightBumperValue);
+
+            // Check A button
+            const isAButtonPressed = gamepad.buttons[1].pressed;
+            updateAButtonImage(isAButtonPressed);
+
+            // Check B button
+            const isBButtonPressed = gamepad.buttons[2].pressed;
+            updateBButtonImage(isBButtonPressed);
+
+            // Check C button
+            const isCButtonPressed = gamepad.buttons[4].pressed;
+            updateCButtonImage(isCButtonPressed);
+
+            // Check X button
+            const isXButtonPressed = gamepad.buttons[0].pressed;
+            updateXButtonImage(isXButtonPressed);
+
+            // Check Y button
+            const isYButtonPressed = gamepad.buttons[3].pressed;
+            updateYButtonImage(isYButtonPressed);
+
+            // Check Z button
+            const isZButtonPressed = gamepad.buttons[5].pressed;
+            updateZButtonImage(isZButtonPressed);
+
+            // Check Start button
+            const isStartButtonPressed = gamepad.buttons[9].pressed;
+            updateStartButtonImage(isStartButtonPressed);
+
+            // Check Select button
+            const isSelectButtonPressed = gamepad.buttons[8].pressed;
+            updateSelectButtonImage(isSelectButtonPressed);
+
+            // Check D-pad Up button
+            updateDPadUpButton(YAxisValue === -1 && XAxisValue !== 1  && XAxisValue !== -1);
+
+            // Check D-pad Up Right button
+            updateDPadUpRightButton(XAxisValue === 1 && YAxisValue === -1);
+
+            // Check D-pad Right button
+            updateDPadRightButton(XAxisValue === 1 && YAxisValue !== -1 && YAxisValue !== 1);
+
+            // Check D-pad Down Right button
+            updateDPadDownRightButton(XAxisValue === 1 && YAxisValue === 1);
+
+            // Check D-pad Down button
+            updateDPadDownButton(YAxisValue === 1 && XAxisValue !== 1  && XAxisValue !== -1);
+
+            // Check D-pad Down Left button
+            updateDPadDownLeftButton(XAxisValue === -1 && YAxisValue === 1);
+
+            // Check D-pad Left button
+            updateDPadLeftButton(XAxisValue === -1 && YAxisValue !== -1 && YAxisValue !== 1);
+
+            // Check D-pad Up Left button
+            updateDPadUpLeftButton(XAxisValue === -1 && YAxisValue === -1);
+        }
+    }
+
+// Schedule the next update
+requestAnimationFrame(checkGamepad);
+}
+
+// Listen for the gamepadconnected event
+window.addEventListener('gamepadconnected', handleGamepadConnected);
